cb74b87a6701e7dcfe95c0eef77fee73fe70bcb6
Rupalii542 <rupali0702.becse24@chitkara.edu.in>
2025-02-22 17:14:57 +0530

    Solution to Print a Palindromic Number Triangle

---
+++ Print a Palindromic Number Triangle@@67877e1818d53eb724f435d0/code.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n); // Number of rows in the pyramid
+    for(int i = 1; i <= n; i++) {
+        for(int j = i; j < n; j++) {
+            printf(" ");
+        }
+        for(int j = 1; j <= (2 * i - 1); j++) {
+            printf("%d",n);
+        }
+        printf("\n");
+    }
+    return 0;
+}
