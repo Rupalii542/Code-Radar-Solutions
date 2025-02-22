a0f9076c2bf931572c6b12d2285f8b2aca72842c
Rupalii542 <rupali0702.becse24@chitkara.edu.in>
2025-02-22 16:40:05 +0530

    Solution to Print a Right-Angled Triangle Pattern

--- Print a Right-Angled Triangle Pattern@@67877e1818d53eb724f435bc/code.c
+++ Print a Right-Angled Triangle Pattern@@67877e1818d53eb724f435bc/code.c
@@ -1,28 +1,41 @@
-// Your code here...
+// // Your code here...
+// #include<stdio.h>
+// int main(){
+//     int num;
+//     scanf("%d",&num);
+//     if(num==1){
+//         printf("*");
+//     }
+//     else if(num==2){
+//         printf("* \n* *");
+//     }
+//     else if(num==3){
+//         printf("*\n* *\n* * *");
+//     }
+//     else if(num==4){
+//         printf("*\n* *\n* * *\n* * * *");
+//     }
+//     else if(num==5){
+//         printf("*\n* *\n* * *\n* * * *\n* * * * *");
+//     }
+//     else if(num==6){
+//         printf("*\n* *\n* * *\n* * * *\n* * * * *\n* * * * * *");
+//     }
+//     else if(num==7){
+//         printf("*\n* *\n* * *\n* * * *\n* * * * *\n* * * * * * *\n* * * * * *");
+//     }
+//     return 0;
+// }
+
 #include<stdio.h>
 int main(){
-    int num;
-    scanf("%d",&num);
-    if(num==1){
-        printf("*");
-    }
-    else if(num==2){
-        printf("* \n* *");
-    }
-    else if(num==3){
-        printf("*\n* *\n* * *");
-    }
-    else if(num==4){
-        printf("*\n* *\n* * *\n* * * *");
-    }
-    else if(num==5){
-        printf("*\n* *\n* * *\n* * * *\n* * * * *");
-    }
-    else if(num==6){
-        printf("*\n* *\n* * *\n* * * *\n* * * * *\n* * * * * *");
-    }
-    else if(num==7){
-        printf("*\n* *\n* * *\n* * * *\n* * * * *\n* * * * * * *\n* * * * * *");
+    int n;
+    scanf("%d",&n);
+    for (int i=0; i<n; i++){
+        for(int j=0; j<i-1; j++){
+            printf("*");
+        }
+        printf("\n");
     }
     return 0;
 }
