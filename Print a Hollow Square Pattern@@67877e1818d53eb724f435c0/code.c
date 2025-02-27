8be78838ec778b74786eee13eef39d7b58dcbf7e
Rupalii542 <rupali0702.becse24@chitkara.edu.in>
2025-02-27 17:01:35 +0530

    Solution to Print a Hollow Square Pattern

--- Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
+++ Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
@@ -1,27 +1,45 @@
+// #include<stdio.h>
+// int main(){
+//     int num;
+//     scanf("%d",&num);
+//     if(num==2){
+//         printf("**\n**");
+//     }
+//     else if(num==3){
+//         printf("***\n* *\n***");
+//     }
+//     else if(num==4){
+//         printf("****\n*  *\n*  *\n****");
+//     }
+//     else if(num==5){
+//         printf("*****\n*   *\n*   *\n*   *\n*****");
+//     }
+//     else if(num==6){
+//         printf("******\n*    *\n*    *\n*    *\n*    *\n******");
+//     }
+//     else if(num==7){
+//         printf("*******\n*     *\n*     *\n*     *\n*     *\n*     *\n*******");
+//     }
+// else{
+//     printf("********\n*      *\n*      *\n*      *\n*      *\n*      *\n*      *\n********");
+// }
+//     return 0;
+// }
+
+
 #include<stdio.h>
 int main(){
-    int num;
-    scanf("%d",&num);
-    if(num==2){
-        printf("**\n**");
+    int n;
+    scanf("%d",&n);
+    for(int i=1; i<=n; i++){
+        for(int j=1;j<=n;j++){
+            if(i==1|| i==4 || j==1|| j==4){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
     }
-    else if(num==3){
-        printf("***\n* *\n***");
-    }
-    else if(num==4){
-        printf("****\n*  *\n*  *\n****");
-    }
-    else if(num==5){
-        printf("*****\n*   *\n*   *\n*   *\n*****");
-    }
-    else if(num==6){
-        printf("******\n*    *\n*    *\n*    *\n*    *\n******");
-    }
-    else if(num==7){
-        printf("*******\n*     *\n*     *\n*     *\n*     *\n*     *\n*******");
-    }
-else{
-    printf("********\n*      *\n*      *\n*      *\n*      *\n*      *\n*      *\n********");
-}
-    return 0;
 }
