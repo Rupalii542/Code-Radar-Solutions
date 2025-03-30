56fc187e4c142955d1e6661f50c970e2d4451667
Rupalii542 <rupali0702.becse24@chitkara.edu.in>
2025-03-30 11:13:58 +0530

    Solution to Check if Array is Sorted

--- Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
+++ Check if Array is Sorted@@676c02002dbc8c3e7a280b6f/code.c
@@ -1,34 +1,40 @@
-#include<stdio.h>
-void sort(int arr[], int n){
-    for(int p=0;p<n-1; p++){
-        for(int c=0;c<n-p-1;c++){
-            if(arr[c]>arr[c+1]){
-                int temp=arr[c];
-                arr[c]=arr[c+1];
-                arr[c+1]=temp;
-            }
-        }
-    }
-}
-int isSorted(int arr[], int n) {
+#include <stdio.h>
+
+// Function to check if the array is sorted (Ascending, Descending, or Not Sorted)
+void checkSorted(int arr[], int n) {
+    int isAscending = 1, isDescending = 1; // Flags to check order
+
     for (int i = 0; i < n - 1; i++) {
-        if (arr[i] >= arr[i + 1]) {
-            return 0; 
+        if (arr[i] > arr[i + 1]) {
+            isAscending = 0; // If any element is greater than the next, it's not ascending
+        }
+        if (arr[i] < arr[i + 1]) {
+            isDescending = 0; // If any element is smaller than the next, it's not descending
         }
     }
-    return 1; 
-}
-int main(){
-    int size;
-    scanf("%d",&size);
-    int arr[size];
-    for(int i=0; i<size; i++){
-scanf("%d",&arr[i]);
-    }
-    if (isSorted(arr, size)) {
+
+    // Check the flags
+    if (isAscending) {
         printf("Sorted\n");
+    } else if (isDescending) {
+        printf("not Sorted\n");
     } else {
         printf("Not Sorted\n");
     }
+}
+
+int main() {
+    int size;
+    scanf("%d", &size);
+    int arr[size];
+
+    // Taking input for the array
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Checking the sorting order
+    checkSorted(arr, size);
+
     return 0;
 }
